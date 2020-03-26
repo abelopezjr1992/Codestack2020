6dad798fe0d5349aef3bdc5ff0a43e46fd503bc8
Abraham Lopez <40772224+abelopezjr1992@users.noreply.github.com>
2020-03-26 00:36:48 -0700

    12.

---
+++ LopezAngularRedesign/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
+import { PhotosComponent } from './photos/photos.component';
+import { CateringComponent } from './catering/catering.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'catering', component: CateringComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
